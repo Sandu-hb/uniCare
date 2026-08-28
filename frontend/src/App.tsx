ac175eeb1064a05aa19098773902aef2bfd6c28d
Dhananjaya Chamod <dhananjayahbc.23@uom.lk>
2026-08-28 20:55:09 +0530

    refactor: render the app router from App

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,5 @@
-import { Navigate, Route, Routes } from 'react-router-dom'
-import SystemStatus from '@/pages/SystemStatus'
+import { AppRouter } from '@/app/router'
 
 export default function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<SystemStatus />} />
-      <Route path="*" element={<Navigate to="/" replace />} />
-    </Routes>
-  )
+  return <AppRouter />
 }
