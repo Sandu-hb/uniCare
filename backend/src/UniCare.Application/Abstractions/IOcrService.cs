9c52669da48ec85935f487bde06e28b4f6368200
Dhananjaya Chamod <dhananjayahbc.23@uom.lk>
2026-09-09 12:43:40 +0530

    chore: remove IOcrService abstraction
    AI extraction feature descoped; staff will review uploaded documents
    manually instead.

--- backend/src/UniCare.Application/Abstractions/IOcrService.cs
+++
@@ -1,6 +0,0 @@
-namespace UniCare.Application.Abstractions;
-
-public interface IOcrService
-{
-    Task<string> ExtractTextAsync(Stream fileContent, CancellationToken cancellationToken = default);
-}
